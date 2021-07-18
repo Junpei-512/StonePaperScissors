780fe0d3b76940ade55485f5085a0914592a027f
Junpei-512 <junpei.910512.sunsys.cst@gmail.com>
2021-07-18 14:33:33 +0900

    Initial Commit

---
+++ StonePaperScissorsApp/StonePaperScissorsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StonePaperScissorsAppApp.swift
+//  StonePaperScissorsApp
+//
+//  Created by Murayama Junpei on 2021/07/18.
+//
+
+import SwiftUI
+
+@main
+struct StonePaperScissorsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
